34a722e7ab3b79a9ba8aeb53d10131d97604637d
eric_ray <947523418@qq.com>
2020-01-20 00:03:27 +0800

    integrate mybatis to insert user

--- src/main/java/citi/eric/community/controller/AuthorizeController.java
+++ src/main/java/citi/eric/community/controller/AuthorizeController.java
@@ -2,6 +2,8 @@ package citi.eric.community.controller;
 
 import citi.eric.community.dto.AccessTokenDTO;
 import citi.eric.community.dto.GithubUser;
+import citi.eric.community.mapper.UserMapper;
+import citi.eric.community.model.User;
 import citi.eric.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
@@ -27,6 +30,8 @@ public class AuthorizeController {
     @Value("${github.redirect.uri}")
     private String redirectUri;
 
+    @Autowired
+    private UserMapper userMapper;
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code")String code,
                            @RequestParam(name = "state")String state,
@@ -38,17 +43,24 @@ public class AuthorizeController {
         accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser user = githubProvider.getUser(accessToken);
-        if(user != null){
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        if(githubUser != null){
+            User user = new User();
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setName(githubUser.getName());
+            user.setToken(UUID.randomUUID().toString());
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
 
-            System.out.println(user.getId());
+            System.out.println(githubUser.getId());
 
             //登陆成功，写Cookie和Session
-            request.getSession().setAttribute("user",user);
-            return "redirect:index";
+            request.getSession().setAttribute("user",githubUser);
+            return "redirect:/";
         }else{
             //登陆失败
-            return "redirect:index";
+            return "redirect:/";
         }
     }
 
