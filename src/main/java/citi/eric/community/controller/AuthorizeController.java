f59d7a48b1f6a1a0b69c81aa37c6044457c57258
Eric <947523418@qq.com>
2020-03-09 00:20:12 +0800

    adding publish

--- src/main/java/citi/eric/community/controller/AuthorizeController.java
+++ src/main/java/citi/eric/community/controller/AuthorizeController.java
@@ -12,7 +12,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 @Controller
@@ -35,7 +37,7 @@ public class AuthorizeController {
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code")String code,
                            @RequestParam(name = "state")String state,
-                           HttpServletRequest request){
+                           HttpServletRequest request,HttpServletResponse response){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
         accessTokenDTO.setClient_id(clientId);
@@ -48,7 +50,8 @@ public class AuthorizeController {
             User user = new User();
             user.setAccountId(String.valueOf(githubUser.getId()));
             user.setName(githubUser.getName());
-            user.setToken(UUID.randomUUID().toString());
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
             userMapper.insert(user);
@@ -56,7 +59,7 @@ public class AuthorizeController {
             System.out.println(githubUser.getId());
 
             //登陆成功，写Cookie和Session
-            request.getSession().setAttribute("user",githubUser);
+            response.addCookie(new Cookie("token",token));
             return "redirect:/";
         }else{
             //登陆失败
--- src/main/java/citi/eric/community/mapper/UserMapper.java
+++ src/main/java/citi/eric/community/mapper/UserMapper.java
@@ -3,9 +3,18 @@ package citi.eric.community.mapper;
 import citi.eric.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
+
+
+    @Select("SELECT * FROM user WHERE token=#{token}")
+    User findUserByToken(@Param("token") String token);
+
     @Insert("INSERT INTO user (account_id,name,token,gmt_create,gmt_modified) values (#{accountId},#{name},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
+
+
 }
